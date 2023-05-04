3a9373fa1aae49014d864b83fa0a654cd064d01e
Bressendorff <95355589+Bressendorff@users.noreply.github.com>
2023-05-04 12:12:45 +0200

    Product Details, Categories & Seeding more products

---
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -0,0 +1,27 @@
+﻿namespace BlazorEcommerce.Client.Services.ProductService
+{
+	public class ProductService : IProductService
+	{
+		private readonly HttpClient _http;
+
+		public ProductService(HttpClient http)
+		{
+			_http = http;
+		}
+		public async Task GetProducts()
+		{
+			var result = 
+				await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/Product");
+			if (result != null && result.Data != null) Products = result.Data;
+		}
+
+        public async Task<ServiceResponse<Product>> GetProduct(int id)
+        {
+            var result =
+                await _http.GetFromJsonAsync<ServiceResponse<Product>>($"api/Product/{id}");
+            return result;
+        }
+
+        public List<Product> Products { get; set; }
+	}
+}
